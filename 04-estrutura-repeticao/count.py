2af1c2523dc71f4867e2306ea399482aaf64d9ba
Aurelio Malheiros <aureliomalheiros@gmail.com>
2020-12-28 20:32:51 -0300

    estrutura de repeticao

---
+++ 04-estrutura-repeticao/count.py
@@ -0,0 +1,4 @@
+count = 1
+while count <= 5:
+    print(f'Valor: ', count)
+    count+=1
---
+++ 04-estrutura-repeticao/sair.py
@@ -0,0 +1,8 @@
+prompt = "\nVocê quer sair?"
+prompt = "\nPressione enter para continuar e 'quit' para sair\n"
+
+messagem = ""
+
+while messagem != 'quit':
+    messagem = input(prompt)
+    print("Tchau amigo")
